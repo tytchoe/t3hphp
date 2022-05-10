b216c5a60e5d9c622d495a8e5be43236b6f03212
Tùng.PT <jeniferchoe2712@gmail.com>
2022-05-10 21:50:20 +0700

    update all

--- Buoi3/layouts/index.php
+++ Buoi3/layouts/index.php
@@ -29,13 +29,7 @@
     <section id="sidebar">
 
         <!-- Intro -->
-        <section id="intro">
-            <a href="#" class="logo"><img src="images/logo.jpg" alt="" /></a>
-            <header>
-                <h2>Future Imperfect</h2>
-                <p>Another fine responsive site template by <a href="http://html5up.net">HTML5 UP</a></p>
-            </header>
-        </section>
+        <?php include_once 'intro.php' ?>
 
         <!-- Mini Posts -->
         <?php include_once  'mini_posts.php';?>
