2bdd984aa0a70bfd01d086dfc8d50671a9ba4249
Tùng.PT <jeniferchoe2712@gmail.com>
2022-05-10 20:35:49 +0700

    update

--- Buoi3/function.php
+++ Buoi3/function.php
@@ -1,3 +1,18 @@
 <?php
+    function show_name(){
+        echo 'hello world'.'<br>';
+    }
+    show_name();
 
+    function show_name_var($param){
+        echo $param.'<br>';
+    }
+    show_name_var('hello world 2');
+
+    function show_full_name($name, $age, $address = 'HN'){
+        echo "Ten : $name , tuoi : $age , dia chi : $address ".'<br>';
+    }
+    show_full_name('dung','20','px');
+
+    function show
 ?>
